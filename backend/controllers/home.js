fe05c36c1e807ca18999115fa86f70f55c99b965
Shruti3004 <mail2shruti.ag@gmail.com>
2021-07-20 18:09:23 +0530

    fixed the home backend
    Signed-off-by: Shruti3004 <mail2shruti.ag@gmail.com>

--- backend/controllers/home.js
+++ backend/controllers/home.js
@@ -3,51 +3,49 @@ const mime = require("mime");
 const fs = require("fs");
 
 exports.addHomeData = async (req, res) => {
-  var matches = req.body.featureOne.match(/^data:([A-Za-z-+/]+);base64,(.+)$/)
-  var matchesTwo = req.body.featureTwo.match(/^data:([A-Za-z-+/]+);base64,(.+)$/)
-  var matchesThree = req.body.featureThree.match(/^data:([A-Za-z-+/]+);base64,(.+)$/)
-  response = {};
-  if (matches.length !== 3 && matchesTwo.length !== 3 && matchesThree.length !==3) {
+  var matches = req.body.featureOne.match(/^data:([A-Za-z-+/]+);base64,(.+)$/);
+  var matchesTwo = req.body.featureTwo.match(
+    /^data:([A-Za-z-+/]+);base64,(.+)$/
+  );
+  var matchesThree = req.body.featureThree.match(
+    /^data:([A-Za-z-+/]+);base64,(.+)$/
+  );
+  var response = {};
+  if (
+    matches.length !== 3 &&
+    matchesTwo.length !== 3 &&
+    matchesThree.length !== 3
+  ) {
     return res.status(400).send({
       msg: "Invalid Image",
     });
   }
   response.type = matches[1];
-    response.data = new Buffer(matches[2], "base64");
-    let decodedImg = response;
-    let imageBuffer = decodedImg.data;
-    let type = decodedImg.type;
-    let extension = mime.extension(type);
-    let fileName = req.body.featureOne.replace(/\s/g, "") + "." + extension;
+  response.data = new Buffer(matches[2], "base64");
+  let decodedImg = response;
+  let imageBuffer = decodedImg.data;
+  let type = decodedImg.type;
+  let extension = mime.extension(type);
+  let fileName = "featureOne" + "." + extension;
+  console.log(fileName);
+  try {
     fs.writeFileSync("./assets/images/" + fileName, imageBuffer, "utf8");
     let product = {
       ...req.body,
-      img: "images/" + fileName,
+      featureOne: "images/" + fileName,
+      featureTwo: "images/" + fileName,
+      featureThree: "images/" + fileName,
     };
+    console.log(product);
     let homePage = HomePage(product);
-  if (
-    !req.body.heading ||
-    !req.body.subHeading ||
-    !req.body.curriculaHeading ||
-    !req.body.curriculaSubHeading ||
-    !req.body.discoverHeading ||
-    !req.body.discoverSubHeading ||
-    !req.body.discoverText ||
-    !req.body.kindergarten ||
-    !req.body.discoverMoreHeading ||
-    !req.body.discoverMoreSubHeading ||
-    !req.body.discoverMoreText ||
-    !req.body.featureOne ||
-    !req.body.featureTwo ||
-    !req.body.featureThree
-  ) {
-    return res.status(400).json({ msg: "Invalid data" });
+    homePage.save((err, data) => {
+      return res.status(201).json(data);
+    });
+  }catch(e){
+    return res.status(400).send({
+      msg: e,
+    });
   }
-
-  let data = HomePage(req.body);
-  homePage.save((err, data) => {
-    return res.status(201).json(data);
-  });
 };
 
 exports.modifyHomeData = async (req, res) => {

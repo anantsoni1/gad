9d7fb181985d5eefa3bca4d7516f2a01784d36a0
Ayush Kumar <2580ayush2580@gmail.com>
2021-07-21 17:28:08 +0530

    admission admin-frontend done

--- frontend/src/components/addmission/index.js
+++ frontend/src/components/addmission/index.js
@@ -4,7 +4,6 @@ import { imageUrl } from "../../redux/api/index";
 import { GetAdmissionData } from "../../redux/actions/admission";
 import Header from "../header/index";
 import Group8921 from "../../assets/Group8921.svg";
-import image22 from "../../assets/image22.jpg";
 import { Link } from "react-router-dom";
 import "./style.css";
 
@@ -36,49 +35,21 @@ const Addmission = () => {
           <div className="container">
             <div className="row pt-lg-5 pt-4 pb-1 px-lg-4 px-md-5">
               <div className="col-12 col-md-12 col-lg-12 col-sm-12 col-xs-12 px-lg-4 pt-md-3 pt-sm-5 pt-xs-5 px-md-5 px-4">
-                <p className="h2">Applying to G.A.D. Elementary School</p>
+                <p className="h2">{formData?.heading}</p>
               </div>
             </div>
             <div className="row pt-1 px-lg-4 px-md-5">
               <div className="col-12 col-md-12 col-lg-7 col-sm-12 col-xs-12 px-lg-4 py-md-3 py-sm-5 py-xs-5 px-md-5 px-4">
                 <p className="about-text">
-                  <p>
-                    Thank you for your interest in G.A.D. Elementary School. We
-                    understand that selecting a school for their child is a
-                    significant decision for parents. We offer an excellent
-                    educational experience in a very conducive environment,
-                    where students excel in their academic, athletic and
-                    artistic pursuits.
-                  </p>
-                  <p>
-                    Children seeking admission from Grade 1 to 9 will have to
-                    take a written Math and English placement test.
-                  </p>
-                  <p>
-                    Note: We also run a Preschool in the campus and accept
-                    children who are three years old. The admission to Preschool
-                    is ongoing. For more information please contact the main
-                    school office (604-595-0888) or visit to see the Preschool
-                    in action.
-                  </p>
-                  <p>
-                    We invite all applying families to visit our school while
-                    school is in session. We offer tours of the school to
-                    families on working days. Interested families may please
-                    contact the front desk to schedule a family visit.
-                  </p>
-                  <p>
-                    The following information must be submitted with the
-                    application: At GAD, We make our admissions process as
-                    simple and as straight forward as possible.
-                  </p>
+                  <p>{formData?.paragraph1}</p>
+                  <p>{formData?.paragraph2}</p>
                 </p>
               </div>
               <div className="align-items-center d-flex col-12 col-md-12 col-lg-5 col-sm-12 col-xs-12 px-lg-2 py-md-3 py-sm-2 py-xs-2 py-2 px-md-5 px-4">
                 <img
                   className="img-responsive-addmission addmissionHeadingImage"
-                  src={image22}
-                  alt={image22}
+                  src={fileOne}
+                  alt={fileOne}
                 ></img>
               </div>
               <div className="row pt-lg-5 pt-4 pb-1 px-lg-4 px-md-5">
@@ -89,54 +60,92 @@ const Addmission = () => {
                   <p className="about-text">
                     <p className="pt-2">
                       <span className="font-weight-bold">STEP 1. </span>{" "}
-                      Complete the Application Form
+                      {formData?.step1Text}
                     </p>
                     <p>
-                      <Link href="/">
-                        Click here to download New Application Form
-                      </Link>
+                      {formData?.step1Links && formData?.step1Links.length > 0
+                        ? formData?.step1Links.map((val, index) => {
+                            return (
+                              <>
+                                <p>
+                                  <Link to={val.link}>{val.text}</Link>
+                                </p>
+                              </>
+                            );
+                          })
+                        : ""}
                     </p>
-                    <p>
-                      <Link href="/">
-                        Click here to download Re-enrollment Form
-                      </Link>
-                    </p>
-                    <p>Sign and date it.</p>
+                    <p></p>
+                    {formData?.step1Points && formData?.step1Points.length > 0
+                      ? formData?.step1Points.map((val, index) => {
+                          return <p>{val.point}</p>;
+                        })
+                      : ""}
                     <p className="pt-4">
-                      <span className="font-weight-bold">STEP 2.</span> Make
-                      payment of $350 ( non-refundable & non-transferable
-                      Registration Fee) payable to G.A.D. Elementary School
-                      (re-enrollment registration fee is $200)
+                      <span className="font-weight-bold">STEP 2.</span>
+                      {formData?.step2Text}
                     </p>
                     <p>
                       <ul>
-                        <li>By Cash</li>
-                        <li>By Credit card</li>
+                        {formData?.step2Points &&
+                        formData?.step2Points.length > 0
+                          ? formData?.step2Points.map((val, index) => {
+                              return <li>{val.point}</li>;
+                            })
+                          : ""}
                       </ul>
+                    </p>
+                    <p>
+                      {formData?.step2Links && formData?.step2Links.length > 0
+                        ? formData?.step2Links.map((val, index) => {
+                            return (
+                              <>
+                                <p>
+                                  <Link to={val.link}>{val.text}</Link>
+                                </p>
+                              </>
+                            );
+                          })
+                        : ""}
                     </p>
                     <p className="pt-4">
                       <span className="font-weight-bold">STEP 3. </span>
-                      Attach the following documents with application Form
+                      {formData?.step3Text}
                     </p>
                     <p>
                       <ul>
-                        <li>Parents’ Legal Residency Form</li>
-                        <li>Health Alert Form</li>
-                        <li>A Copy of Child’s Birth Certificate</li>
-                        <li>Child’s Medical Card</li>
-                        <li>For Kindergarten only: Immunization record</li>
-                        <li>
-                          Citizenship Cards/PR Cards/Immigrant Visa of the
-                          parent
-                        </li>
-                        <li>Document with proof of Address</li>
+                        {formData?.step3Points &&
+                        formData?.step3Points.length > 0
+                          ? formData?.step3Points.map((val, index) => {
+                              return <li>{val.point}</li>;
+                            })
+                          : ""}
                       </ul>
+                    </p>
+                    <p>
+                      {formData?.step3Links && formData?.step3Links.length > 0
+                        ? formData?.step3Links.map((val, index) => {
+                            return (
+                              <>
+                                <p>
+                                  <Link to={val.link}>{val.text}</Link>
+                                </p>
+                              </>
+                            );
+                          })
+                        : ""}
                     </p>
                     <p className="py-5">
                       For More information click here to see{" "}
-                      <Link>admission policy</Link> & Tuition Fee & Donation
-                      Policy
-                      <Link> click here.</Link>
+                      {formData?.links && formData?.links.length > 0
+                        ? formData?.links.map((val, index) => {
+                            return (
+                              <>
+                                <Link to={val.link}>{val.text} </Link>
+                              </>
+                            );
+                          })
+                        : ""}
                     </p>
                   </p>
                 </div>

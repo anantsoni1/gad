8fa350d86338f704d096ed2ec2d04b06049b5cf4
Karan Bains <karanbains.bains@gmail.com>
2021-07-23 21:35:54 +0530

    added delete route

--- backend/routes/parents.js
+++ backend/routes/parents.js
@@ -4,6 +4,9 @@ const parents = require("../controllers/parents");
 router.post("/addSchoolPolicy", parents.addSchoolPolicyData);
 router.post("/addSchoolNewsletter", parents.addSchoolNewsletterData);
 router.post("/addCalendar", parents.addCalendarData);
+router.delete("/deleteSchoolPolicy", parents.deleteSchoolPolicyData);
+router.delete("/deleteSchoolNewsletter", parents.deleteSchoolNewsletterData);
+router.delete("/deleteCalendar", parents.deleteCalendarData);
 // router.post("/addParentsData", parents.addParentsData);
 
 module.exports = {

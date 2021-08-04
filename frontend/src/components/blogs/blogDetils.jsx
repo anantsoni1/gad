0db973445ee972ef7fd6bd7e9c71a1098d122b2d
Karan Bains <karanbains.bains@gmail.com>
2021-08-04 16:54:31 +0530

    completed blog details

--- frontend/src/components/blogs/blogDetils.jsx
+++ frontend/src/components/blogs/blogDetils.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { imageUrl } from "../../redux/api/index";
-import { getBlogs } from "../../redux/actions/blogs";
+import { getBlogsById } from "../../redux/actions/blogs";
 import Header from "../header/index";
 import Group8921 from "../../assets/Group8921.svg";
 import { Link, useHistory, useLocation } from "react-router-dom";
@@ -12,16 +12,29 @@ const Addmission = () => {
     const location = useLocation();
     const id = new URLSearchParams(location.search).get("id");
     console.log(id);
-    const blogs = useSelector(state => {return state?.blogs?.getBlogsData?.data});
+    const blog = useSelector(state => {return state?.blogs?.getBlogById?.data});
+    const date = new Date(blog?.date);
     const dispatch = useDispatch();
     useEffect(() => {
-        dispatch(getBlogs())
+        dispatch(getBlogsById(id))
     }, []);
   return (
     <>
           <h1 id="covid" className=" text-center center">
             <div>
-              <div className="text-white main-heading">BLOG DETAILS</div>
+              <div className="text-white main-heading">{blog?.title}</div>
+              <div className="text-center text-white mt-5 sub-heading carousel-flex">
+                {/* <div className="text-white text-center px-lg-0 mt-md-0 mt-3 mx-3 text-underline pb-2"
+                style={{"cursor": "pointer"}}>
+                  QUICK LINKS: COVID-19 Resources
+                </div> */}
+                <div className="text-white text-center px-lg-0 mt-md-0 mx-3 pb-2">
+                  By : <span style={{'fontWeight' : 'bold'}}>{blog?.author} </span>
+                </div>
+                <div className="text-white text-center px-lg-0 mt-md-0 mx-3 pb-2">
+                  {date.getDate()} {date.toLocaleString('default', { month: 'short' })} {date.getFullYear()}
+                </div>
+              </div>
               <div className="text-center text-white mt-5 sub-heading carousel-flex">
                 {/* <div className="text-white text-center px-lg-0 mt-md-0 mt-3 mx-3 text-underline pb-2"
                 style={{"cursor": "pointer"}}>
@@ -32,12 +45,12 @@ const Addmission = () => {
                  onClick={()=>{
                    history.push('/blogs')
                  }}>
-                  View All Blogs
+                  View All Blogs and Articles
                 </div>
               </div>
             </div>
           </h1>
-          <div>
+          <div dangerouslySetInnerHTML={{__html:blog?.desc}} className="col-lg-10 col-md-10 col-sm-12 col-12 mx-auto mt-5">
 
           </div>
     </>
